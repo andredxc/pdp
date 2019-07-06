d77d98fa5c29668d771815b5ada10168d7dda3a3
André Dexheimer Carneiro <andre.dxc@hotmail.com>
2019-07-06 17:52:10 -0300

    ^

--- python/sem_prodcons.py
+++ python/sem_prodcons.py
@@ -1,6 +1,7 @@
+import sys
 import threading
 
-NUM_THREADS = 16
+NUM_THREADS = 0
 PRODUCT = "2" * 30
 
 _production = None
@@ -8,8 +9,20 @@ _semaphores = None
 
 def main():
 
-    # Inicializa o vetor de producao
+    # Verifica argumentos
+    try:
+        if len(sys.argv) != 2 or not isinstance(int(sys.argv[1]), int):
+            print("Usage: python sem_prodcons.py <num_threads>")
+            return
+    except ValueError:
+        print("Usage: python sem_prodcons.py <num_threads>")
+        return
+
+    # Inicializa argumentos passados por linha de comando
     global _production, _semaphores, NUM_THREADS
+    NUM_THREADS = int(sys.argv[1])
+
+    # Inicializa o vetor de producao
     _production = [""] * NUM_THREADS
     _semaphores = [threading.Semaphore()] * NUM_THREADS
 
@@ -17,7 +30,7 @@ def main():
     producerThreads = []
     consumerThreads = []
     for i in range(NUM_THREADS):
-        print("Starting threads %d" % i)
+        print("Starting thread %d" % i)
         producerThreads.append(threading.Thread(target=producer, args=(i,)))
         consumerThreads.append(threading.Thread(target=consumer, args=(i,)))
 
